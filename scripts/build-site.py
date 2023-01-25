f190710a0afe5a06d49dd64344399cbda5dbc7c8
actions-user <actions-user@users.noreply.github.com>
2023-01-25 11:16:09 +0000

    Include third-party versions in report

--- scripts/build-site.py
+++ scripts/build-site.py
@@ -14,6 +14,7 @@ import subprocess
 EXCLUDE_LIST = ["Arimo", "Cousine", "Tinos"]
 
 results = json.load(open("docs/noto.json"))
+versions = json.load(open("docs/versions.json"))
 
 
 def _basename(this, item):
@@ -54,16 +55,38 @@ helpers = {
     "denoto": _denoto,
 }
 
+def icon_for_platform(platform):
+    if "Android" in platform:
+        return f'<span class="material-icons" data-toggle="tooltip" title="{platform}"> android </span>'
+    if "iOS" in platform:
+        return f'<span class="material-icons" data-toggle="tooltip" title="{platform}"> phone_iphone </span>'
+    if "macOS" in platform:
+        return f'<span class="material-icons" data-toggle="tooltip" title="{platform}"> laptop_mac </span>'
+    if "Google Fonts" in platform:
+        return f'<img data-toggle="tooltip" title="{platform}" src="gflogo.png" width=18 height=18></img>'
+    if "Fedora" in platform:
+        return f'<img data-toggle="tooltip" title="{platform}" src="fedora.png" width=18 height=18></img>'
+    return platform
+
 compiler = Compiler()
 template = open("scripts/template.html", "r").read()
 template = compiler.compile(template)
 
 for result in results.values():
     result["has_releases"] = False
-    for family in result.get("families", []).values():
+    for family_name, family in result.get("families", []).items():
         if family.get("latest_release"):
             result["has_releases"] = True
-            break
+            latest_version = family["latest_release"]["version"][1:]
+            if family_name in versions:
+                family["third_party_versions"] = { 
+                    k: {
+                        "version": versions[family_name][k],
+                        "up_to_date": versions[family_name][k] == latest_version,
+                        "icon": icon_for_platform(k)
+                    }
+                    for k in sorted(versions[family_name].keys())
+                }
     result["issue_count"] = len(result["issues"])
     result["families_count"] = len(result["families"])
 
@@ -80,3 +103,7 @@ bug_template = compiler.compile(bug_template)
 output = bug_template({"results": results})
 with open("docs/reporter.html", "w") as fh:
     fh.write(output)
+
+json.dump(
+    results, open("debug.json", "w"), indent=True, sort_keys=True
+)
